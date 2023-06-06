321e241cc348e3db67709c24e6ab83ffdc4867f7
Slawek Derabin <slawomirderabin@gmail.com>
2023-06-06 10:48:13 +0200

    add function

--- app.js
+++ app.js
@@ -32,50 +32,101 @@ const prepareOutputFilename = (filename) => {
   return `${name}-with-watermark.${ext}`;
 };
 
+const editImage = async (imagePath) => {
+  const image = await Jimp.read(imagePath);
+
+  const editOptions = await inquirer.prompt([{
+    name: 'edit',
+    type: 'confirm',
+    message: 'Do you want to edit the image?',
+    default: false
+  }]);
+
+  if (!editOptions.edit) {
+    return image;
+  }
+
+  const modifications = await inquirer.prompt([{
+    name: 'modification',
+    type: 'list',
+    message: 'Choose a modification:',
+    choices: [
+      'Make image brighter',
+      'Increase contrast',
+      'Make image black and white',
+      'Invert image'
+    ]
+  }]);
+
+  if (modifications.modification === 'Make image brighter') {
+    image.brightness(0.3);
+  } else if (modifications.modification === 'Increase contrast') {
+    image.contrast(0.3);
+  } else if (modifications.modification === 'Make image black and white') {
+    image.greyscale();
+  } else if (modifications.modification === 'Invert image') {
+    image.invert();
+  }
+
+  return image;
+};
+
 const startApp = async () => {
 
   // Ask if user is ready
   const answer = await inquirer.prompt([{
-      name: 'start',
-      message: 'Hi! Welcome to "Watermark manager". Copy your image files to `/img` folder. Then you\'ll be able to use them in the app. Are you ready?',
-      type: 'confirm'
-    }]);
+    name: 'start',
+    message: 'Hi! Welcome to "Watermark manager". Copy your image files to `/img` folder. Then you\'ll be able to use them in the app. Are you ready?',
+    type: 'confirm'
+  }]);
 
   // if answer is no, just quit the app
-  if(!answer.start) process.exit();
+  if (!answer.start) process.exit();
 
-  // ask about input file and watermark type
-  const options = await inquirer.prompt([{
+  // ask about input file
+  const inputOptions = await inquirer.prompt([{
     name: 'inputImage',
     type: 'input',
     message: 'What file do you want to mark?',
-    default: 'test.jpg',
-  }, {
-    name: 'watermarkType',
-    type: 'list',
-    choices: ['Text watermark', 'Image watermark'],
+    default: 'test.jpg'
   }]);
 
-  if(options.watermarkType === 'Text watermark') {
+  const editedImage = await editImage('./img/' + inputOptions.inputImage);
+
+  // ask about watermark type
+  const options = await inquirer.prompt([{
+    name: 'watermarkType',
+    type: 'list',
+    choices: ['Text watermark', 'Image watermark']
+  }]);
+
+  if (options.watermarkType === 'Text watermark') {
     const text = await inquirer.prompt([{
       name: 'value',
       type: 'input',
-      message: 'Type your watermark text:',
-    }])
+      message: 'Type your watermark text:'
+    }]);
     options.watermarkText = text.value;
-    addTextWatermarkToImage('./img/' + options.inputImage, './img/' + prepareOutputFilename(options.inputImage), options.watermarkText);
-  }
-  else {
+    addTextWatermarkToImage(
+      editedImage,
+      './img/' + prepareOutputFilename(inputOptions.inputImage),
+      options.watermarkText
+    );
+  } else {
     const image = await inquirer.prompt([{
       name: 'filename',
       type: 'input',
       message: 'Type your watermark name:',
-      default: 'logo.png',
-    }])
+      default: 'logo.png'
+    }]);
     options.watermarkImage = image.filename;
-    addImageWatermarkToImage('./img/' + options.inputImage, './img/' + prepareOutputFilename(options.inputImage), './img/' + options.watermarkImage);
+    addImageWatermarkToImage(
+      editedImage,
+      './img/' + prepareOutputFilename(inputOptions.inputImage),
+      './img/' + options.watermarkImage
+    );
   }
 
 };
 
-startApp();
+startApp()
